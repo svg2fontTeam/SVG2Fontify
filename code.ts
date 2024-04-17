90b565a3e10036b7ea4da864493431a171696cc7
‘yoon-jisung’ <hmsj2380@gmail.com>
2024-04-18 00:50:19 +0900

    add: eslint

--- code.ts
+++ code.ts
@@ -2,8 +2,8 @@
 // the *figma document* via the figma global object.
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
-
 // Runs this code if the plugin is run in Figma
+
 if (figma.editorType === 'figma') {
   // This plugin will open a window to prompt the user to enter a number, and
   // it will then create that many rectangles on the screen.
