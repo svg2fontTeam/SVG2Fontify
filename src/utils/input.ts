e177b042aa39433ded80c39e15beb6c6132dfa0b
dolphin159 <dolphin00@naver.com>
2024-04-27 01:26:31 +0900

    add: 피그마 input 유효성 검사 테스트 코드 작성

---
+++ src/utils/input.ts
@@ -0,0 +1,17 @@
+export default function validationChkAction(type: string, isErr: boolean, postVal: string) {
+  const message =
+    type === 'version'
+      ? "Only numbers and '.' are allowed as input"
+      : "Special characters such as (~,!,#,$,%,^,&,*,(,),_,|,+,\\,-,=,?,;,:,',\",<,>,{,},[,],/,' ') are not allowed.";
+  const regexp =
+    type === 'version'
+      ? /[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣`~!@#$%^&*()_|+\-=,?;:'"<>{}[\]\\/\s]/gim
+      : /[`~!#$%^&*()_|+\-=?;:'"<>{}[\]\\/\s]/gim;
+  if (regexp.test(postVal)) {
+    const rtnVal = postVal.replace(regexp, '');
+    figma.notify(message, { error: isErr, timeout: 2000 });
+    return rtnVal;
+  }
+
+  return postVal;
+}
