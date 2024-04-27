348dd6d9a413800c77099ac8c343d2d287778a5d
zzinao <kjn0265@gmail.com>
2024-04-27 14:43:35 +0900

    refactor: test 코드 위치 수정

--- src/__tests__/versionPage.test.ts
+++ src/__tests__/versionPage.test.ts
@@ -16,11 +16,6 @@ describe('createPage', () => {
     expect(newPage.name).toBe(pageTitle);
     expect(newPage.backgrounds[0].type).toBe('SOLID');
   });
-
-  it('선택된 요소가 없으면 에러를 내뱉는다', () => {
-    const mockPage = { currentPage: { selection: [] } } as unknown as PluginAPI;
-    expect(() => getselectedItems(mockPage)).toThrowError('파일이 선택되지 않았습니다.');
-  });
 });
 
 describe('getselectedItems', () => {
@@ -35,6 +30,11 @@ describe('getselectedItems', () => {
     },
   ];
   const mockPage = { currentPage: { selection: selectedItem } } as unknown as PluginAPI;
+
+  it('선택된 요소가 없으면 에러를 내뱉는다', () => {
+    const mockPage = { currentPage: { selection: [] } } as unknown as PluginAPI;
+    expect(() => getselectedItems(mockPage)).toThrowError('파일이 선택되지 않았습니다.');
+  });
 
   it('복사한 아이템들을 배열로 return한다.', () => {
     expect(getselectedItems(mockPage)).not.toBe(mockPage.currentPage.selection);
