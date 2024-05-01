2099a8488d015f33fb28a52eaaed5017a91d123c
‘yoon-jisung’ <hmsj2380@gmail.com>
2024-05-01 23:12:50 +0900

    add: drag data push figma ui

--- src/utils/drag.ts
+++ src/utils/drag.ts
@@ -1,59 +1,47 @@
 export default async function drag() {
   const nodes = figma.currentPage.selection;
+
   if (nodes.length < 0) {
     console.log('선택된 노드가 없습니다.');
     return;
   }
 
-  if (
-    [
-      'FRAME',
-      'COMPONENT',
-      'VECTOR',
-      'BOOLEAN_OPERATION',
-      'STAR',
-      'LINE',
-      'ELLIPSE',
-      'POLYGON',
-      'RECTANGLE',
-      'TEXT',
-    ].includes(node.type)
-  ) {
-    // const svgs = await Promise.all(
-    //   nodes.map(async (node) => {
-    //     let name;
-    //     if (node.parent) {
-    //       name = `${node.parent.name}_${node.name.split('=').join('_')}`;
-    //     } else {
-    //       name = node.name;
-    //     }
-    //     const svgTag = await node.exportAsync({ format: 'SVG' });
-    //     return { name, svg: svgTag };
-    //   })
-    // );
-    // console.log('SVG 데이터:', svgs);
-  } else {
-    console.log('SVG로 내보낼 수 없는 노드 타입:', node);
+  const filterSvg = nodes.filter(
+    (el) => el.type === 'VECTOR' || el.type === 'FRAME' || el.type === 'COMPONENT'
+  );
+  const isNotSvg = nodes.filter(
+    (el) => el.type !== 'VECTOR' && el.type !== 'FRAME' && el.type !== 'COMPONENT'
+  );
+
+  if (isNotSvg.length > 0) {
+    console.log(isNotSvg.map((el) => el.type));
+    console.log('백터가 아닌 노드가 있습니다.');
+    return;
   }
 
-  // figma.ui.onmessage = (msg: { type: string }) => {
-  //   if (msg.type === 'get-selected-svgs') {
-  //     const selection = figma.currentPage.selection;
-  //     const svgs = selection.filter(
-  //       (node: SceneNode) => node.type === 'VECTOR' || node.type === 'FRAME'
-  //     );
+  const svgs = await Promise.all(
+    filterSvg.map(async (node) => {
+      const svgFile = await node.exportAsync({ format: 'SVG' });
+      return { name: node.name, svg: svgFile };
+    })
+  );
+  console.log(svgs);
+  figma.ui.postMessage({ type: 'selected-svgs', svgs: svgs });
 
-  //     // SVG 요소들의 정보를 UI로 전송
-  //     figma.ui.postMessage({ type: 'selected-svgs', svgs: svgs });
-  //   }
-  // };
+  window.onmessage = (event: MessageEvent) => {
+    console.log(event.data.pluginMessage);
+    const { type, svgs } = event.data.pluginMessage as { type: string; svgs: SceneNode[] };
+    if (type === 'selected-svgs') {
+      const countBadge = document.getElementById('count-badge');
 
-  // // UI 스크립트에서는 아래와 같이 메시지를 받을 수 있음
-  // window.onmessage = (event: MessageEvent) => {
-  //   const { type, svgs } = event.data.pluginMessage as { type: string; svgs: SceneNode[] };
-  //   if (type === 'selected-svgs') {
-  //     console.log('Selected SVGs:', svgs);
-  //     // 이곳에서 SVG 데이터를 처리하거나 사용자에게 보여줄 수 있음
-  //   }
-  // };
+      if (!countBadge) {
+        console.error('Element with id "count-badge" not found.');
+        return;
+      }
+      countBadge.textContent = svgs.length.toString();
+      countBadge.style.display = 'flex';
+
+      // 이곳에서 SVG 데이터를 처리하거나 사용자에게 보여줄 수 있음
+    }
+  };
 }
