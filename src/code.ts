70498fc16331ec3092415c6ab1fefb2b8f52ee26
zzinao <77870077+zzinao@users.noreply.github.com>
2024-05-07 17:48:55 +0900

    refactor: 타입, 상수 분리

--- src/code.ts
+++ src/code.ts
@@ -1,28 +1,42 @@
-import drag from './utils/drag';
-import { generateSVGCode, iconToFont, svgsToSvgFont } from './utils/generate';
+import { generateSVGCode, iconToFont } from './utils/generate';
 import validationChkAction from './utils/input';
 import { createVersionPage } from './utils/versionPage';
+import drag from './utils/drag';
+import { PluginMessageEnum } from './constants';
 
 figma.showUI(__html__, { width: 360, height: 640 });
 console.log('플러그인이 시작되었습니다.');
 
 figma.ui.onmessage = async (msg: { type: string; isErr: boolean; postVal: string }) => {
-  console.log(msg, 'msg@@@@');
-  const rtnVal = validationChkAction(msg.type, msg.isErr, msg.postVal);
-  figma.ui.postMessage({ type: 'INPUT', data: rtnVal });
+  console.log('ON_MSG : ', msg);
 
-  const svgList = await generateSVGCode(figma);
-  const test = await iconToFont(svgList);
-  console.log('TEST:', test);
+  if (msg.type === PluginMessageEnum.SUBMIT) {
+    const rtnVal = validationChkAction(msg.type, msg.isErr, msg.postVal);
+    figma.ui.postMessage({ type: 'INPUT', data: rtnVal });
 
-  figma.ui.postMessage({
-    type: 'save-iconfont',
-    data: { svgs: svgList, fontName: 'fontName', ...test },
-  });
+    // TODO: 추후 version, fontName input 데이터로 분기
+    const version = false;
+    const fontName = 'fontName';
+    if (version) {
+      createVersionPage('title', figma);
+    }
+
+    const svgList = await generateSVGCode(figma);
+    const fontOptions = {
+      fontName,
+      fontHeight: 1000,
+      normalize: true,
+    };
+    const fontStream = await iconToFont(svgList, fontOptions);
+
+    figma.ui.postMessage({
+      type: PluginMessageEnum.SAVE_ICONFONT,
+      data: { svgs: svgList, fontName, ...fontStream },
+    });
+  }
 };
 
 figma.on('selectionchange', () => {
-  console.log('selectChange');
   drag();
 });
 
